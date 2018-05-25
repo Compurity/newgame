ac89eb30d9b58d92c3ed911d0af0ec5c0b753b2e
Matt32819 <39490594+Matt32819@users.noreply.github.com>
2018-05-25 10:26:17 -0400

    Update Waves.java

--- Waves.java
+++ Waves.java
@@ -1,33 +1,28 @@
 import java.util.ArrayList;
 public class Waves
 {
-	
 	private int gold;
 	private boolean waveStop;
 	private boolean waveStart;
 	private int waves = 50;
 	private int sum = 0;
+	private int currentWave = 0;
 	
-	for(int i = 0, i < allEnemies.length, i++)
+	public void Spawn()
 	{
-		Enemies(i).Spawn();
+		//Spawns in creeps in each wave
 	}
 	
 	public boolean waveEnd()
 	{
-		for(int j = 0, j < waves, j++)
-		{
-			sum = sum + allEnemies(i).getHealth();
-		}
-		if(sum == 0)
-			return true;
-		return false;
+		//constantly checks if the sum of all enemies is 0. once true, brungs up upgrade page.
 	}
 	public boolean upgrades()
 	{
-		if(waveEnd == true)
-		{
-			
-		}
+		//brings up upgrade window with upgrades to health, mana and abilities.
+	}
+	public int getCurrentWave()
+	{
+		return currentWave //used for stats of enemies in Enemies class.
 	}
 }
