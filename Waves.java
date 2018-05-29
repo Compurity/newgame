589e2cb47ee75bfafa0c44a731ee71ccdfeda647
Matt32819 <39490594+Matt32819@users.noreply.github.com>
2018-05-29 10:26:23 -0400

    Update Waves.java

--- Waves.java
+++ Waves.java
@@ -7,22 +7,47 @@ public class Waves
 	private int waves = 50;
 	private int sum = 0;
 	private int currentWave = 0;
+	Enemy[] enemies = new Enemy[130];
 	
-	public void Spawn()
+	public void Spawn()									//Spawns in creeps in each wave
 	{
-		//Spawns in creeps in each wave
+		int wave = currentWave;
+		for(int i = 0, i < 30 + (wave * 2), i++)		
+		{
+			enemies[i] = new Enemy();
+		}	
+	}	
+	public boolean waveEnd()							//constantly checks if the sum of all enemies is 0. if 0 return true otherwise return false.
+	{
+		int sum = 0;									
+		for(int i = 0, i < enemies.length, i++)
+		{
+			sum += enemies[i].getHealth();
+		}
+		if(sum == 0)
+		{
+			return true;
+		}
+		return false;
 	}
-	
-	public boolean waveEnd()
+	public boolean upgrades()							//brings up upgrade window with upgrades to health, mana and abilities if waveEnd is true.
 	{
-		//constantly checks if the sum of all enemies is 0. once true, brungs up upgrade page.
+		if(waveEnd().equals true)					
+		{
+			
+		}								
 	}
-	public boolean upgrades()
+	public int getCurrentWave()							//used for stats of enemies in Enemy class.
 	{
-		//brings up upgrade window with upgrades to health, mana and abilities.
-	}
-	public int getCurrentWave()
-	{
-		return currentWave //used for stats of enemies in Enemies class.
+		int sum = 0;
+		for(int i = 0, i < enemies.length, i++)			
+		{
+			sum += enemies[i].getHealth();
+		}
+		if(sum == 0)
+		{
+			currentWave += 1;
+		}
+		return currentWave
 	}
 }
