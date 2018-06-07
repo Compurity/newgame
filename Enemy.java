7c6694593bd1bcdadeb096e363160261f95e0a1f
Compurity <wings4free@gmail.com>
2018-06-06 22:43:36 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -7,30 +7,27 @@ public class Enemy
 	private int speed;
 	private int mana;
 	
-	public int X;
-	public int Y;
-	public int H;
-	public int W;
+	public int X1;
+	public int Y1;
+	public int H1;
+	public int W1;
 	
 	public Enemy()
 	{
-	 	health = 200 + (20 * Waves.getCurrentWave());
-	 	damage = 25 + (2 * Waves.getCurrentWave());
+	 	//health = 200 + (20 * Waves.getCurrentWave());
+	 	//damage = 25 + (2 * Waves.getCurrentWave());
 	 	attSpeed = 1;
 	 	speed = 20;
-	 	mana = 100 + (5 * Waves.getCurrentWave());
+	 	//mana = 100 + (5 * Waves.getCurrentWave());
+	 	X1 = 10;
+	 	Y1 = 10;
+	 	H1 = 10;
+	 	W1 = 10;
 	}
 	
 	public Enemy(int a, int b, double c, int d, int e)
 	{
-		health = a;
-		damage = b;
-		attSpeed = c;
-		speed = d;
-		mana = e;
+		
 	}
-	public int getHealth()
-	{
-		return health;
-	}
+	
 }
