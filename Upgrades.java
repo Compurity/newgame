78658aa8b959f60341df162d0e5f60abe3786f3f
Matt32819 <39490594+Matt32819@users.noreply.github.com>
2018-06-08 18:49:43 -0400

    Update Upgrades.java

--- Upgrades.java
+++ Upgrades.java
@@ -1,40 +1,48 @@
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-
+import javax.swing.JTextField;
 public class Upgrades extends JFrame {
-
+	
 	private static final long serialVersionUID = 1L;
-
 	public Upgrades() {
-
 		// set flow layout for the frame
 		this.getContentPane().setLayout(new FlowLayout());
-
 		JButton button1 = new JButton();
-		button1.setText("Java Code Geeks");
-
+		button1.setText("Finished");
 		JButton button2 = new JButton("Java Examples & Code Snippets");
-
+		//////////////////////////////////
+		//JFrame f = new JFrame("Button Example");  
+		final JTextField tf=new JTextField();  
+		tf.setBounds(50,50, 150,20);  
+		JButton b=new JButton("Click Here");  
+		b.setBounds(50,100,95,30);
+		b.addActionListener(new ActionListener(){  
+			public void actionPerformed(ActionEvent e){  
+				tf.setText("Welcome to Javatpoint.");  
+			}  
+		});
+		/////////////////////////////////
 		// add buttons to frame
 		add(button1);
 		add(button2);
-
+		add(b);
+		add(tf);
+		
 	}
-
+	/*public void actionPerformed(ActionEvent e){  
+        tf.setText("Welcome to Javatpoint.");  
+    } */
 	public static void createAndShowGUI() {
-
   //Create and set up the window.
-
   JFrame frame = new Upgrades();
-
+  frame.setBounds(500,100,500,500);
   //Display the window.
-
   frame.pack();
-
   frame.setVisible(true);
-
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
